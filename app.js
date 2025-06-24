8a9352818f61326ce8d6a531eba5c91680985dc8
ak2004-star <officialadityasingh21@gmail.com>
2025-06-25 01:46:52 +0530

    updated

---
+++ app.js
@@ -0,0 +1,5 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+const ele = React.createElement("h1",{},"Hello World");
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(ele);
